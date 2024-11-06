9727bfd3e3e36f66bac97a398eccfd90fb46b6ba
m1guelzin <miguelgarridodev@gmail.com>
2024-11-06 08:32:40 -0300

    att Conexao sql

--- api/src/routes/apiRoutes.js
+++ api/src/routes/apiRoutes.js
@@ -1,13 +1,12 @@
 const router = require('express').Router()
 
-const { route } = require('..');
 const userController = require("../controllers/userController")
 const orgController = require("../controllers/orgController")
 
 router.post('/user', userController.createUser);
 //router.post('user/login', userController.postLogin);
 router.get('/user', userController.getAllUsers);
-//router.get('/user/:cpf', userController.gerUserById); 
+//router.get('/user/:cpf', userController.getxUserById); 
 router.put('/user', userController.updateUser);
 router.delete('/user/:id', userController.deleteUser);
 
