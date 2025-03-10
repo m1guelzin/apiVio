df9a82c31b186b4b96d1985740662db82d6d41a2
m1guelzin <miguelgarridodev@gmail.com>
2025-03-10 10:56:15 -0300

    att./ banco de dados com VIEW

--- api/src/db/connect.js
+++ api/src/db/connect.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     connectionLimit:10,
     host:'localhost',
     user:'alunods',
-    password:'senai@604',
+    password:'miguel4559',
     database:'vio_miguel'
 })
 
