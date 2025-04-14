157e31956246be691747b0c212425061046a99e6
m1guelzin <miguelgarridodev@gmail.com>
2025-04-14 14:18:55 -0300

    att./ functions-procedures vcd

--- mysql-init/functions.sql
+++ mysql-init/functions.sql
@@ -87,3 +87,167 @@ end; $$
 delimiter ;
 
 select mensagem_boas_vindas('Miguelzin')
+
+select routine_name from
+information_schema.routines
+    where routine_type = 'FUNCTION'
+        and routine_schema = 'vio_miguel';
+
+
+-- maior idade
+delimiter $$
+
+create function is_maior_idade(data_nascimento date)
+returns boolean
+not deterministic
+contains sql
+begin
+    declare idade int;
+
+    -- utilizando a função ja criada
+    set idade = calcula_idade(data_nascimento);
+    return idade >= 18;
+end; $$
+
+delimiter ;
+
+-- categorizar usuários por faixa etária
+delimiter $$
+
+create function faixa_etaria(data_nascimento date)
+returns varchar(20)
+not deterministic
+contains sql
+begin
+    declare idade int;
+
+    -- calculo da idade com a função existente
+    set idade = calcula_idade(data_nascimento);
+    if idade < 18 then
+        return "Menor de idade";
+    elseif idade < 60 then
+        return "Adulto"; 
+    elseif idade >= 60 then
+        return "Idoso";
+    else 
+        return "Idade não encontrada";
+end if; 
+end; $$
+
+delimiter ;
+
+-- agrupar usuarios por faixa etária 
+select faixa_etaria(data_nascimento) as faixa,
+count(*) as quantidade from usuario
+group by faixa;
+
+-- identificar uma faixa etartia especifica
+select name from usuario
+    where faixa_etaria(data_nascimento) = "Adulto";
+
+-- "timestampdiff" diferença de time
+
+-- calcular a média de idade de usuarios cadastrados
+delimiter $$
+create function media_idade()
+returns decimal(5,2)
+not deterministic
+reads sql data
+begin
+    declare media decimal(5,2);
+    
+    -- cálculo da média das idades
+    select avg(timestampdiff(year, data_nascimento, curdate())) into media from usuario;
+
+    return ifnull(media, 0);
+end; $$
+delimiter ;
+
+-- selecionar idade especifica
+select "A média de idade dos clientes é maior que 30" as resultado where media_idade() > 30;
+
+
+-- EXERCICIO DIRECIONAS DE FUNCTIONS/PROCEDURES --------------------------------------------------------------------------------------------
+-- cálculo do total de gasto por um usuário
+delimiter $$
+create function calcula_total_gasto(pid_usuario int)
+returns decimal(10, 2)
+not deterministic
+reads sql data
+begin
+    declare total decimal (10, 2);
+
+    select sum(i.preco * ic.quantidade) into total
+    from compra c
+    join ingresso_compra ic on c.id_compra = ic.fk_id_compra
+    join ingresso i on i.id_ingresso = ic.fk_id_ingresso
+    where c.fk_id_usuario = pid_usuario;
+
+    return ifnull(total, 0);
+
+    end; $$
+    delimiter ;
+
+-- buscar a faita etaria do usuario
+delimiter $$
+create function buscar_faixa_etaria_usuario(pid int)
+returns varchar(20)
+not deterministic
+reads sql data
+begin
+    declare nascimento date;
+    declare faixa varchar(20);
+
+    select data_nascimento into nascimento
+    from usuario
+    where id_usuario = pid;
+
+    set faixa = faixa_etaria(nascimento);
+
+    return faixa;
+end; $$
+delimiter ;
+
+-- EXERCICIOO >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+-- Função para ver todos os ingressos vendidos no evento
+delimiter $$
+
+create function total_ingressos_vendidos(id_evento int)
+returns int
+reads sql data
+begin
+    declare total int;
+
+    select ifnull(sum(ic.quantidade), 0)
+    into total
+    from ingresso_compra ic
+    join ingresso i on ic.fk_id_ingresso = i.id_ingresso
+    where i.fk_id_evento = id_evento;
+
+    return total;
+end $$
+
+delimiter ;
+
+-- Função para ver a renda total do evento
+delimiter $$
+
+create function renda_total_evento(id_evento int)
+returns decimal(10,2)
+reads sql data
+begin
+    declare total decimal(10,2);
+
+    select ifnull(sum(i.preco * ic.quantidade), 0.00)
+    into total
+    from ingresso_compra ic
+    join ingresso i on ic.fk_id_ingresso = i.id_ingresso
+    where i.fk_id_evento = id_evento;
+
+    return total;
+end $$
+
+delimiter ;
+
+-- Codigo ip da maquina 10.89.240.82
--- mysql-init/procedures.sql
+++ mysql-init/procedures.sql
@@ -1,27 +1,26 @@
 delimiter //
-
 create procedure registrar_compra(
-    in p.id_usuario int,
-    in p.id_ingresso int,
-    in p.quantidade int
+    in p_id_usuario int,
+    in p_id_ingresso int,
+    in p_quantidade int
 )
 begin
     declare v_id_compra int;
 
     -- criar registro na tabela 'compra'
-    insert into compra( data_compra, fk_id_usuario)
-    values(now(), p.id_usuario);
+    insert into compra(data_compra, fk_id_usuario)
+    values(now(), p_id_usuario);
 
-    -- obter o ID da compra recem-criada
-    set v_id_compra = last_insert_id()
+    -- obter o ID da compra recém-criada
+    set v_id_compra = last_insert_id();
 
     -- registrar os ingressos comprados
     insert into ingresso_compra(fk_id_compra, fk_id_ingresso, quantidade)
-    values(v_id_compra, p.id_ingresso, p.quantidade);
+    values(v_id_compra, p_id_ingresso, p_quantidade);
+end;
+//
+delimiter ;
 
-    end; //
-
-    delimiter ;
 
 delimiter //
 
@@ -65,5 +64,49 @@ delimiter ;
 call registrar_presenca(1, 3);  
 drop procedure registrar_presenca;
 
---
+-- PROCEDURE PARA O RESUMO DO USUÁRIO (ATIVIDADE)
+delimiter $$
 
+create procedure resumo_usuario(in pid int)
+begin
+    declare nome varchar(100);
+    declare email varchar(100);
+    declare totalrs decimal(10,2);
+    declare faixa varchar(20);
+
+-- busca o nome e o email do usuario
+    select u.name, u.email into nome, email
+    from usuario u
+    where u.id_usuario = pid;
+
+    -- chamda das funções especificas ja criadas
+    set totalrs = calcula_total_gasto(pid);
+    set faixa = buscar_faixa_etaria_usuario(pid);
+
+    -- Mostra os dados formatados
+    select nome as nome_usuario,
+    email as email_usuario,
+    totalrs as total_gasto,
+    faixa as faixa_etaria;
+
+end; $$
+
+delimiter ;
+
+-- EXERCICIOOO >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+delimiter //
+
+create procedure resumo_evento(in id_evento int)
+begin
+    select 
+        e.nome as nome_evento,
+        date(e.data_hora) as data_evento,
+        total_ingressos_vendidos(e.id_evento) as total_vendidos,
+        renda_total_evento(e.id_evento) as renda_total
+    from evento e
+    where e.id_evento = id_evento;
+end; //
+
+delimiter ;
+
+-- Codigo ip da maquina 10.89.240.82
